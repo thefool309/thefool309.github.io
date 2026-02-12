530a33a2b7cafac70f93a89f3bf3b49824f2ad2b
thefool309 <themagician309@gmail.com>
2026-02-11 22:10:42 -0600

    added CivSculptorView

---
+++ src/views/CivSculptorView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import ProjectPage from '@/components/ProjectPage.vue'
+</script>
+
+<template>
+  <div class="fastapi-view">
+    <ProjectPage title="civ-sculptor" content1="" />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .fastapi-view {
+    min-height: 95vh;
+    display: flex;
+    align-items: center;
+  }
+  a {
+    color: var(--color-hyperlink);
+  }
+}
+</style>

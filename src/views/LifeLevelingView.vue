c8362c48af8d3a35fb0efa665f331831a190998b
thefool309 <themagician309@gmail.com>
2026-02-12 18:10:56 -0600

    changed tense of verbiage in title on lifeleveling page

--- src/views/LifeLevelingView.vue
+++ src/views/LifeLevelingView.vue
@@ -7,8 +7,8 @@ import ProjectPage from '@/components/ProjectPage.vue'
     <ProjectPage
       title="Life Leveling"
       content1="LifeLeveling is a gamified self-care and personal growth application focused on helping users build positive habits through progression systems and rewards. I maintain the project repository and contribute across the backend and infrastructure, designing systems for user tracking, progression, and scalability. I also handle DevOps responsibilities, including deployment workflows and environment configuration, to ensure the project remains reliable and maintainable as it evolves."
-      content2-head="Who Worked On This Project?"
-      content2="Our frontend is handled by Elysia, and StephenC1993. The backend systems are mostly architectured and implemented by fdesouza1992. I handle DevOps and take a support role in the backend development. I am working with Stephen to flesh out a graphics system for this program."
+      content2-head="Who Works On This Project?"
+      content2="Our frontend is handled by Elysia, and StephenC1993. The backend systems are mostly architectured and implemented by fdesouza1992. This is an Open Source project and contributions are welcome. I handle DevOps and take a support role in the backend development. I am working with Stephen to flesh out a graphics system for this program."
       image1="/Life_Leveling_4.png"
       alt1="The life leveling logo"
       github_url="https://github.com/thefool309/lifeleveling"
